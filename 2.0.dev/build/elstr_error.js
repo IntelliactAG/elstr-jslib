8a72600ba6cf3d5927f5506be92f2f9b812f1c79
Marco Egli <eglinetwork@users.noreply.github.com>
2011-05-31 14:03:20 +0000

    New error function Y.ELSTR.error.unhandledException(e);

--- 2.0.dev/build/elstr_error.js
+++ 2.0.dev/build/elstr_error.js
@@ -10,38 +10,41 @@ YUI.add('elstr_error', function(Y) {
     Y.ELSTR.error = {
         // public properties or functions
         requestFailure : function (oRequest, oResponse, oPayload, oDataSource, oCallback){
-			var status = oResponse.status;
-			var responseText =  oResponse.responseText;
-			//console.log(oResponse);
+            var status = oResponse.status;
+            var responseText =  oResponse.responseText;
+            //console.log(oResponse);
 			
-			try {
-				var parsedResponse = Y.JSON.parse(responseText);
-			}
-			catch (e) {
-				Y.ELSTR.utils.log(e,"error");
-				Y.ELSTR.utils.log("Request: " + oRequest,"info");
-				return;
-			}
+            try {
+                var parsedResponse = Y.JSON.parse(responseText);
+            }
+            catch (e) {
+                Y.ELSTR.utils.log(e,"error");
+                Y.ELSTR.utils.log("Request: " + oRequest,"info");
+                return;
+            }
 			 
-			switch(status) {
-				case 401:
-					var enterpriseApplication = parsedResponse.error.data.context;
+            switch(status) {
+                case 401:
+                    var enterpriseApplication = parsedResponse.error.data.context;
 
-					if (Y.Lang.isUndefined( Y.ELSTR.user.enterpriseApplicationAuthEvent[enterpriseApplication] )){
-						Y.ELSTR.user.login(enterpriseApplication);
-					}
-					Y.ELSTR.user.enterpriseApplicationAuthEvent[enterpriseApplication].subscribe(function(type, args){
-						oDataSource.sendRequest(oRequest, oCallback);
-					});
-					break;
+                    if (Y.Lang.isUndefined( Y.ELSTR.user.enterpriseApplicationAuthEvent[enterpriseApplication] )){
+                        Y.ELSTR.user.login(enterpriseApplication);
+                    }
+                    Y.ELSTR.user.enterpriseApplicationAuthEvent[enterpriseApplication].subscribe(function(type, args){
+                        oDataSource.sendRequest(oRequest, oCallback);
+                    });
+                    break;
 				
-				default:
-					Y.ELSTR.utils.log("Request failed!","error");
-					Y.ELSTR.utils.log("Status: " + status,"info");
-					Y.ELSTR.utils.log("Response: " + responseText,"info");
-			}
-			
-		}
+                default:
+                    Y.ELSTR.utils.log("Request failed!","error");
+                    Y.ELSTR.utils.log("Status: " + status,"info");
+                    Y.ELSTR.utils.log("Response: " + responseText,"info");
+            }			
+        },
+        unhandledException : function(e){
+            Y.ELSTR.utils.log(e,"error");
+            Y.ELSTR.utils.log("Unhandled Exception","info");            
+        }
     }
  
 }, '2.0.0' /* module version */, {
