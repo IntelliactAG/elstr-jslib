5fae9c226add60f4ef31ead3cbc0d26b8e29bb0f
Marco Egli <eglinetwork@users.noreply.github.com>
2014-06-04 08:09:33 +0000

    Adding public logout function to force logout on specific user behavior

--- 2.0.dev/build/elstr_user.js
+++ 2.0.dev/build/elstr_user.js
@@ -1,4 +1,3 @@
-
 /**
  * Module to privide a Widget and functionallity to handle basic user funtions
  * such as login, logout, admin in Elstr applicationss.
@@ -10,7 +9,7 @@
  * @copyright Intelliact AG, 2011
  */
 
-YUI.add('elstr_user', function (Y) {
+YUI.add('elstr_user', function(Y) {
 
     /**
      * User handling for Elstr applications
@@ -31,27 +30,27 @@ YUI.add('elstr_user', function (Y) {
      * @param config {Object} Configuration object
      */
     Y.namespace('ELSTR').User = Y.Base.create('elstr_user', Y.Widget, [], {
-                
+
         //
         // WIDGET FUNCTIONS
         //
-        
+
         /**
          * Designated initializer
          *
          * @method initializer
          */
-        initializer: function () {
+        initializer: function() {
             // Init the user object from DOM
             if (Y.Lang.isObject(ELSTR.applicationData.user)) {
-                this._set("username",ELSTR.applicationData.user.username);
-                this._set("isAuth",ELSTR.applicationData.user.isAuth);
-                this._set("isAdmin",ELSTR.applicationData.user.isAdmin);
-                this._set("resourcesAllowed",ELSTR.applicationData.user.resourcesAllowed);
-                this._set("enterpriseApplicationData",ELSTR.applicationData.user.enterpriseApplicationData);
+                this._set("username", ELSTR.applicationData.user.username);
+                this._set("isAuth", ELSTR.applicationData.user.isAuth);
+                this._set("isAdmin", ELSTR.applicationData.user.isAdmin);
+                this._set("resourcesAllowed", ELSTR.applicationData.user.resourcesAllowed);
+                this._set("enterpriseApplicationData", ELSTR.applicationData.user.enterpriseApplicationData);
                 ELSTR.applicationData.user = "empty after reading it to the user widget";
             }
-              
+
             // TODO: implement correct auth handling
             Y.on('elstr_auth:successfulLogout', this._onSuccessfulLogout);
             Y.on('elstr_auth:successfulAuth', this._onSuccessfulAuth);
@@ -65,7 +64,7 @@ YUI.add('elstr_user', function (Y) {
          *
          * @method initializer
          */
-        destructor: function () {
+        destructor: function() {
             // Remove all click listeners
             this.get('contentBox').purge(true);
         },
@@ -76,9 +75,9 @@ YUI.add('elstr_user', function (Y) {
          * The auth UI is allways loaded from markup, never rendered at runtime
          * @method renderUI
          */
-        renderUI: function () {
-        // Always loaded from markup
-        // E.g. srcNode:"#loginHandler"
+        renderUI: function() {
+            // Always loaded from markup
+            // E.g. srcNode:"#loginHandler"
         },
 
         /**
@@ -87,49 +86,52 @@ YUI.add('elstr_user', function (Y) {
          * Hooks up events for the widget
          * @method bindUI
          */
-        bindUI: function () {
+        bindUI: function() {
             var contentBox = this.get('contentBox');
             contentBox.one(".login").on("click", function(e) {
                 e.preventDefault();
                 this._login();
-            },this);
+            }, this);
             contentBox.one(".logout").on("click", function(e) {
                 e.preventDefault();
                 this._logout();
-            },this);
+            }, this);
             contentBox.one(".admin").on("click", function(e) {
                 e.preventDefault();
-                Y.use('elstr_admin', function (Y) {
+                Y.use('elstr_admin', function(Y) {
                     Y.ELSTR.Admin.openAdminConsole();
                 });
             });
         },
 
-        syncUI: function () {
+        syncUI: function() {
             this._updateLoginHandler();
         },
-    
+
         //
         // PUBLIC FUNCTIONS
         //
 
+        logout: function() {
+            this._logout();
+        },
 
         /**
          * Returns if the user has allowed access to a resource
-         * 
+         *
          * @param {string/array} resource Name of a resource
          * @method resourceAllowed
          * @return {Boolean} If the resource is allowed
          */
-        resourceAllowed : function(resource) {
+        resourceAllowed: function(resource) {
             var isAllowed = true,
-            resourcesAllowed = this.get("resourcesAllowed");
+                resourcesAllowed = this.get("resourcesAllowed");
             var objectLiteralOfResourcesAllowed = {};
-            for ( var i = 0; i < resourcesAllowed.length; i++) {
+            for (var i = 0; i < resourcesAllowed.length; i++) {
                 objectLiteralOfResourcesAllowed[resourcesAllowed[i]] = '';
             }
             if (Y.Lang.isArray(resource)) {
-                for ( var j = 0, len = resource.length; j < len; j++) {
+                for (var j = 0, len = resource.length; j < len; j++) {
                     if (!(resource[j] in objectLiteralOfResourcesAllowed)) {
                         isAllowed = false;
                     }
@@ -143,20 +145,19 @@ YUI.add('elstr_user', function (Y) {
         },
         /**
          * Interface for reading enterprise application data
-         * 
+         *
          * @param {string} enterpriseApplication
          * @param {string} key
          * @method getEnterpriseApplicationData
-         * @return 
+         * @return
          */
-        getEnterpriseApplicationData : function(enterpriseApplication, key) {
+        getEnterpriseApplicationData: function(enterpriseApplication, key) {
             var enterpriseApplicationData = this.get("enterpriseApplicationData");
-            if (Y.Lang.isObject(enterpriseApplicationData[enterpriseApplication])){
+            if (Y.Lang.isObject(enterpriseApplicationData[enterpriseApplication])) {
                 var oEnterpriseApplication = enterpriseApplicationData[enterpriseApplication];
-                if (!YAHOO.lang.isUndefined(oEnterpriseApplication[key])){
+                if (!YAHOO.lang.isUndefined(oEnterpriseApplication[key])) {
                     return oEnterpriseApplication[key];
-                }
-                else {
+                } else {
                     return null;
                 }
             } else {
@@ -165,75 +166,75 @@ YUI.add('elstr_user', function (Y) {
         },
         /**
          * Shows a modal access denied Panel
-         * 
+         *
          */
-        showAccessDenied : function(additionalText){
-        //TODO: Not implemented
+        showAccessDenied: function(additionalText) {
+            //TODO: Not implemented
         },
-    
+
         //
         // PRRIVATE VARIABLES
         //
 
-        _auth : null,
+        _auth: null,
 
         //
         // PRRIVATE FUNCTIONS
         //
 
-        _updateLoginHandler : function() {
+        _updateLoginHandler: function() {
             // Update the handler only if it exists
             var contentBox = this.get('contentBox');
-            
-            if(this.get("isAuth") === true){
-                contentBox.one(".login").setStyle("display","none");
-                contentBox.one(".user").empty().append(this.get("username")).setStyle("display","");
-                contentBox.one(".logout").setStyle("display","");
+
+            if (this.get("isAuth") === true) {
+                contentBox.one(".login").setStyle("display", "none");
+                contentBox.one(".user").empty().append(this.get("username")).setStyle("display", "");
+                contentBox.one(".logout").setStyle("display", "");
             } else {
-                contentBox.one(".login").setStyle("display","");
-                contentBox.one(".user").empty().setStyle("display","none");
-                contentBox.one(".logout").setStyle("display","none");
+                contentBox.one(".login").setStyle("display", "");
+                contentBox.one(".user").empty().setStyle("display", "none");
+                contentBox.one(".logout").setStyle("display", "none");
             }
-   
-            if(this.get("isAdmin") === true){
-                contentBox.one(".admin").setStyle("display","");
+
+            if (this.get("isAdmin") === true) {
+                contentBox.one(".admin").setStyle("display", "");
             } else {
-                contentBox.one(".admin").setStyle("display","none");
+                contentBox.one(".admin").setStyle("display", "none");
             }
         },
-        _onSuccessfulAuth : function(result) {
-            this._set("isAuth",result.isAuth);
-            this._set("isAdmin",result.isAdmin);
-            this._set("username",result.username);
-            this._set("resourcesAllowed",result.resourcesAllowed);
-            this._set("enterpriseApplicationData",result.enterpriseApplicationData);
+        _onSuccessfulAuth: function(result) {
+            this._set("isAuth", result.isAuth);
+            this._set("isAdmin", result.isAdmin);
+            this._set("username", result.username);
+            this._set("resourcesAllowed", result.resourcesAllowed);
+            this._set("enterpriseApplicationData", result.enterpriseApplicationData);
             this.syncUI();
         },
-        _onSuccessfulLogout : function() {
-            this._set("isAuth",false);
-            this._set("isAdmin",false);
-            this._set("username","anonymous");
+        _onSuccessfulLogout: function() {
+            this._set("isAuth", false);
+            this._set("isAdmin", false);
+            this._set("username", "anonymous");
             this.syncUI();
             if (this.get("forceAuthentication") === true) {
                 this._login();
             }
         },
-        _createAuthWidget : function(){
+        _createAuthWidget: function() {
             var that = this;
-            Y.use('elstr_auth', function (Y) {
+            Y.use('elstr_auth', function(Y) {
                 that._auth = new Y.ELSTR.Auth({
-                    srcNode:"#loginDialog",
-                    visible:false,
-                    centered:true,
+                    srcNode: "#loginDialog",
+                    visible: false,
+                    centered: true,
                     modal: true,
-                    width:that.get('loginDialogWidth'),
+                    width: that.get('loginDialogWidth'),
                     buttons: [],
-                    forceAuthentication:that.get("forceAuthentication"),
+                    forceAuthentication: that.get("forceAuthentication"),
                     after: {
-                        successfulAuth : function(result){
+                        successfulAuth: function(result) {
                             that._onSuccessfulAuth(result);
                         },
-                        successfulLogout : function(){
+                        successfulLogout: function() {
                             that._onSuccessfulLogout();
                         }
                     }
@@ -242,23 +243,23 @@ YUI.add('elstr_user', function (Y) {
                 that.fire("_authCreated");
             });
         },
-        _login : function(){
-            if(Y.Lang.isNull(this._auth) === true){
+        _login: function() {
+            if (Y.Lang.isNull(this._auth) === true) {
                 this._createAuthWidget();
-                this.after("_authCreated",this._login);
+                this.after("_authCreated", this._login);
             } else {
                 this._auth.login();
             }
         },
-        _logout : function(){
-            if(Y.Lang.isNull(this._auth) === true){
+        _logout: function() {
+            if (Y.Lang.isNull(this._auth) === true) {
                 this._createAuthWidget();
-                this.after("_authCreated",this._logout);
+                this.after("_authCreated", this._logout);
             } else {
                 this._auth.logout();
             }
         }
-        
+
     }, {
         ATTRS: {
             forceAuthentication: {
@@ -298,6 +299,6 @@ YUI.add('elstr_user', function (Y) {
     });
 
 }, '2.0', {
-    requires: ['base','widget','node','elstr_utils'],
+    requires: ['base', 'widget', 'node', 'elstr_utils'],
     skinnable: false
 });
