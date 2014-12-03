e71d3f5e4d7f8bdc60d3de6307bbce7ce4591be8
Andrés Sahún <andystudios@users.noreply.github.com>
2014-12-03 15:30:00 +0000

    Empty test created for the libraries.

---
+++ 3.0.dev/test/ElstrI18n.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sahun on 02.12.2014.
+ */
+
+describe("ElstrI18N", function() {
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
