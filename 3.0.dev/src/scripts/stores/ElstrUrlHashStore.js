97d3b74ac6298acee7f9090ad5b6158e44b1d56a
Marco Egli <eglinetwork@users.noreply.github.com>
2015-02-04 17:02:07 +0000

    Switched back fom === to =

--- 3.0.dev/src/scripts/stores/ElstrUrlHashStore.js
+++ 3.0.dev/src/scripts/stores/ElstrUrlHashStore.js
@@ -26,13 +26,14 @@ function _hashToObject(hash) {
     if (hash){
 
         var e,
-            a = /\+/g,  // Regex for replacing addition symbol with a space
+            a = /\+/g, // Regex for replacing addition symbol with a space
             r = /([^&;=]+)=?([^&;]*)/g,
             d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
             q = hash;
 
-        while (e === r.exec(q))
+        while (e = r.exec(q))
             hashParams[d(e[1])] = d(e[2]);
+
     }
 
     return hashParams;
