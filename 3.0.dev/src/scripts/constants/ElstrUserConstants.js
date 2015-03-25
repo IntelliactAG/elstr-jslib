d2792949f075914b1545165cd5b9ad0b40aea7e6
Andrés Sahún <andystudios@users.noreply.github.com>
2015-03-25 10:01:44 +0000

    peer Dependencies fixed for react 13

--- 3.0.dev/src/scripts/constants/ElstrUserConstants.js
+++ 3.0.dev/src/scripts/constants/ElstrUserConstants.js
@@ -10,9 +10,12 @@ var keyMirror = require('keymirror');
  * User flow constants.
  */
 module.exports = keyMirror({
+
     ELSTR_USER_WILL_LOGIN: null,
     ELSTR_USER_DID_LOGIN_SUCCESS: null,
     ELSTR_USER_DID_LOGIN_FAILED: null,
+
     ELSTR_USER_WILL_LOGOUT: null,
     ELSTR_USER_DID_LOGOUT: null
+
 });
