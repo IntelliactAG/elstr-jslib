59c8460aee4efbac7ae221829cfeae83afc00667
Andrés Sahún <andystudios@users.noreply.github.com>
2015-06-09 06:57:48 +0000

    ElstrEditingStates ElstrLoadingStates

---
+++ 3.0.dev/src/scripts/constants/ElstrEditingStates.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2015, Intelliact AG
+ * Copyrights licensed under the New BSD License
+ * Created by sahun@intelliact on 08.06.2015.
+ */
+
+var keyMirror = require('keymirror');
+
+module.exports = keyMirror({
+
+    NORMAL: null,   // The component has content or is in any Loading state.
+    EDITING: null,  // The user is editing the content.
+    SAVING: null    // The user has finished loading the content, the content is being saved.
+
+});
---
+++ 3.0.dev/src/scripts/constants/ElstrLoadingStates.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2015, Intelliact AG
+ * Copyrights licensed under the New BSD License
+ * Created by sahun@intelliact on 08.06.2015.
+ */
+
+var keyMirror = require('keymirror');
+
+module.exports = keyMirror({
+
+    EMPTY: null,   // Fist time that we load the content, no data. (We're loading) ---> Loading bootstrap bar or inline.
+    CACHED: null,    // We already had data from the cache. (We're loading) ---> Grey non editable text
+    OUTDATED: null,   // External change makes our data outdated. (We're loading) ---> Grey non editable text
+    LOADED: null,    // The data was loaded from the server.
+    CONFLICTED: null    // You're editing something and then you get the notification from ElstrRealTimeUpdate lib that something was changed there.
+
+});
