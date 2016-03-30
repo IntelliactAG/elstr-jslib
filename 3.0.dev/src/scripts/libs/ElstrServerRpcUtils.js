e339de5946a11217cec5d217321f61c5de8cd2fd
MartinBichsel <MartinBichsel@5c050769-0c6e-44ed-8073-5ecc4111bc47>
2016-03-30 07:34:13 +0000

    Handling multiple errors, treating only messages of type error or messages without type as an error.

--- 3.0.dev/src/scripts/libs/ElstrServerRpcUtils.js
+++ 3.0.dev/src/scripts/libs/ElstrServerRpcUtils.js
@@ -6,18 +6,29 @@ var ElstrLog = require('../ElstrLog');
  */
 function ElstrServerRpcUtils (){}
 
-ElstrServerRpcUtils.parseError = function(errorObject) {
+
+ElstrServerRpcUtils.parseErrors = function(messages) {
 
     var errorTxt = "";
 
-    if (errorObject.type){
-        errorTxt = (errorObject.type.toUpperCase()) + ": [" + errorObject.code + "] " + errorObject.message;
-    }else  if (errorObject.message){
-        errorTxt = errorObject.message;
-    }else{
-        errorTxt = (errorObject);
-    }
+    if (messages && messages.length > 0) {
+        for (var i=0;i<messages.length;i++) {
+            var message = messages[i];
 
+            if (message.type) {
+                if (message.type=="error") { // only messages of type error are treated as error
+                    var text = error.text;
+                    if (!text) { text = error.message; }
+                    var additionalMessage = (message.type.toUpperCase()) + ": [" + message.code + "] " +text;
+                    if (errorTxt!=="") { errorTxt = errorTxt + ", " + additionalMessage; }
+                    else               { errorTxt =  additionalMessage; }
+                }
+            } else {  // messages without type error are treated as error
+                if (errorTxt!=="") { errorTxt = errorTxt + ", " + message; }
+                else               { errorTxt =  message; }
+            }
+        }
+    }
     return errorTxt;
 
 };
@@ -33,12 +44,12 @@ ElstrServerRpcUtils.validateMessages = function(data) {
 
     } else if (data.result && data.result.messages && data.result.messages.length > 0) {
 
-        error = ElstrServerRpcUtils.parseError(data.result.messages[0]);
+        error = ElstrServerRpcUtils.parseErrors(data.result.messages);
 
         /* We check for the base object info */
     } else if (data.messages && data.messages.length > 0) {
 
-        error = ElstrServerRpcUtils.parseError(data.messages[0]);
+        error = ElstrServerRpcUtils.parseErrors(data.messages);
 
         /* We check for the base object info */
     }
