c6117b5769e506dc936e97161ab12e54e7b8bca5
Marco Egli <eglinetwork@users.noreply.github.com>
2016-11-03 15:17:40 +0000

    Added detection for Edge

--- 3.0.dev/src/scripts/stores/ElstrEnvStore.js
+++ 3.0.dev/src/scripts/stores/ElstrEnvStore.js
@@ -18,9 +18,9 @@ function _get_browser_info(){
         tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
         return 'IE '+(tem[1]||'');
     }
-    if(M[1]==='Chrome'){
-        tem=ua.match(/\bOPR\/(\d+)/)
-        if(tem!=null)   {return 'Opera '+tem[1];}
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
     }
     M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
     if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
