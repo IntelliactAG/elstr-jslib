8c8be6546f47f56d33d55556a744f9ddb8536544
guitar-hero <geo-me@hotmail.com>
2017-10-09 17:01:03 +0200

    simplified getting base in _setHashObjectWithBasename

--- 3.0.dev/src/scripts/actions/ElstrUrlHashActions.js
+++ 3.0.dev/src/scripts/actions/ElstrUrlHashActions.js
@@ -79,17 +79,7 @@ function _setHashObject( newObject , updateHistory, throwEvent){
 function _setHashObjectWithBasename (newObject) {
 
     let newHash = _serialize(newObject);
-
-    let baseURI = document.baseURI;
-    let base = baseURI.split('/');
-    // remove first 3 elements
-    base.splice(0, 3);
-    // remove top element
-    base.pop();
-    // create the string for the base
-    base = `/${base.join('/')}`;
-    // complete basename with the appName
-    base = `${base}/${LIBS.appName}`;
+    let base = document.location.pathname;
 
     location.replace(`${base}#${newHash}`);
 }
