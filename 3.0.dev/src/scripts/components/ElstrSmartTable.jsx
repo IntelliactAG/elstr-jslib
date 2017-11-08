6739b77c01aac01a11635d06b613a9e6a37b54eb
Andrés Sahún <andystudios@gmail.com>
2017-11-08 18:03:57 +0100

    Fixed bug in IE 11
    Not suported ... notation or Object.assign

--- 3.0.dev/src/scripts/components/ElstrSmartTable.jsx
+++ 3.0.dev/src/scripts/components/ElstrSmartTable.jsx
@@ -12,7 +12,12 @@ require('../libs/ElstrFunctionsControl');
 require ("../../css/ElstrSmartTable.css");
 require ("../../css/ElstrTableDefaultLayout.css");
 
-React.__spread = Object.assign;
+if (Object.assign) {
+    React.__spread = Object.assign;
+}else { // <= IE 11
+    React.__spread = function(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+}
+
 
 /**
  * Renders the content of a single row.
